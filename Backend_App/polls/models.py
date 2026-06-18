2caad2cea81c1c8eec81feef15eca4751f1fe0ea
Liguit John Vincent <johnvincentmanalastasliguit@gmail.com>
2026-06-18 11:47:01 +0800

    manual django view

--- Backend_App/polls/models.py
+++ Backend_App/polls/models.py
@@ -1,19 +1,21 @@
-from datetime import datetime, timezone
+import datetime
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date =  models.DateTimeField("date published")
+    time_treshold = 1
     
     def __str__(self):
         return self.question_text
     
         # ...
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return  timezone.now() <= self.pub_date + datetime.timedelta(days=1) <= timezone.now() + datetime.timedelta(days=1)
     
     
 class Choice (models.Model):
